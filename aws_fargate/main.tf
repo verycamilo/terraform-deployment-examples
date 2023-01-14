06f4b9802f56875259b9c52d7c30ee2b5a43c2e1
Camilo Martinez <camilo.m@verypossible.com>
2023-01-13 22:54:46 -0500

    Add aws_fargate folder = test

---
+++ aws_fargate/main.tf
@@ -0,0 +1,15 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "{{ cookiecutter.cluster_name }}"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "example" {
+  cluster_name = aws_ecs_cluster.cluster.name
+
+  capacity_providers = ["{{ cookiecutter.cluster_capacity_provider }}"]
+
+  default_capacity_provider_strategy {
+    base              = var.capacity_provider_strategy_base
+    weight            = var.capacity_provider_strategy_weight
+    capacity_provider = "{{ cookiecutter.cluster_capacity_provider }}"
+  }
+}
---
+++ aws_fargate/variables.tf
@@ -0,0 +1,9 @@
+variable "capacity_provider_strategy_base" {
+  description = "Capacity provider strategy base"
+  default = 1
+}
+
+variable "capacity_provider_strategy_weight" {
+  description = "Capacity provider strategy weight"
+  default =  100
+}
